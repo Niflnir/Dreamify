e0c559dd0b359d15a30ade81596c11a6e53d75c7
Niflnir <shaunchenggy@gmail.com>
2023-12-29 14:55:34 +0800

    feat: update database operations to use transactions

--- backend/database/posts.go
+++ backend/database/posts.go
@@ -29,48 +29,135 @@ func ListPosts(db *sql.DB) []Post {
   return posts
 }
 
-func CreatePost(db *sql.DB, title string, body string) {
-	_, err := db.Exec("INSERT INTO posts(title,body) VALUES($1,$2)", title, body)
-	if err != nil {
+func CreatePost(db *sql.DB, title string, body string) (Post, error) {
+  // Start transaction
+  tx, err := db.Begin()
+  if err != nil {
 		log.Error().Err(err)
-	} else {
-		log.Info().Msgf("Post with title '%s' and body '%s' created successfully!\n", title, body)
-	}
+    return Post{}, err
+  }
+
+  var p Post
+  {
+    stmt, err := tx.Prepare("INSERT INTO posts(title,body) VALUES($1,$2) RETURNING id, title, body, date_created")
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+
+    defer stmt.Close()
+
+    err = stmt.QueryRow(title, body).Scan(&p.Id, &p.Title, &p.Body, &p.DateCreated)
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+  }
+
+  // Commit transaction
+  {
+    err := tx.Commit()
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+  }
+
+  log.Info().Msgf("Post with title '%s' and body '%s' created successfully!\n", title, body)
+
+  return p, err
 }
 
 func DeletePost(db *sql.DB, id int) (Post, error) {
-  postToDelete := getPostById(id, db)
-
-  _, err := db.Exec("DELETE FROM posts WHERE id = $1", id)
-	if err != nil {
+  // Start transaction
+  tx, err := db.Begin()
+  if err != nil {
 		log.Error().Err(err)
-	} else {
-		log.Info().Msgf("Post with id '%d' deleted successfully!\n", id)
-	}
+    return Post{}, err
+  }
 
-  return postToDelete, err
+  var p Post
+  {
+    stmt, err := tx.Prepare("DELETE FROM posts WHERE id = $1 RETURNING id, title, body, date_created")
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+
+    defer stmt.Close()
+
+    err = stmt.QueryRow(id).Scan(&p.Id, &p.Title, &p.Body, &p.DateCreated)
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+  }
+
+  // Commit transaction
+  {
+    err := tx.Commit()
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+  }
+
+	log.Info().Msgf("Post with id '%d' deleted successfully!\n", id)
+
+  return p, err
 }
 
 func UpdatePost(db *sql.DB, id int, title string, body string) (Post, error) {
-  existingPost := getPostById(id, db)
+  existingPost, err := getPostById(id, db)
+  if err != nil {
+    log.Error().Err(err)
+    return Post{}, err
+  }
 
+  // Do not the field if no value is provided
   if title == "" {
     title = existingPost.Title
   }
-
   if body == "" {
     body = existingPost.Body
   }
 
-  _, err := db.Exec("UPDATE posts SET title=$1, body=$2 WHERE id=$3", title, body, id)
-	if err != nil {
+  // Start transaction
+  tx, err := db.Begin()
+  if err != nil {
 		log.Error().Err(err)
-	} else {
-		log.Info().Msgf("Post with title '%s' and body '%s' updated successfully!\n", title, body)
-	}
+    return Post{}, err
+  }
 
-  updatedPost := getPostById(id, db)
-  return updatedPost, err
+  var p Post
+  {
+    stmt, err := tx.Prepare("UPDATE posts SET title=$1, body=$2 WHERE id=$3")
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+
+    defer stmt.Close()
+
+    err = stmt.QueryRow(title, body, id).Scan(&p.Id, &p.Title, &p.Body, &p.DateCreated)
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+  }
+
+  // Commit transaction
+  {
+    err := tx.Commit()
+    if err != nil {
+      log.Error().Err(err)
+      return Post{}, err
+    }
+  }
+
+	log.Info().Msgf("Post with title '%s' and body '%s' updated successfully!\n", title, body)
+
+  return p, err
 }
 
 func rowsToPost(rows *sql.Rows) Post {
@@ -85,16 +172,13 @@ func rowsToPost(rows *sql.Rows) Post {
   return post
 }
 
-func getPostById(id int, db *sql.DB) Post {
+func getPostById(id int, db *sql.DB) (Post, error) {
   row := db.QueryRow("SELECT * from posts where id=$1", id)
 
-  var post Post 
+  var p Post 
   var err error
 
-  err = row.Scan(&post.Id, &post.Title, &post.Body, &post.DateCreated)
-  if err != nil {
-    log.Error().Err(err)
-  }
+  err = row.Scan(&p.Id, &p.Title, &p.Body, &p.DateCreated)
 
-  return post
+  return p, err
 }
