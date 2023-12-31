bfc3269943f68b854fd52856e1594804a587b749
Niflnir <shaunchenggy@gmail.com>
2023-12-31 10:06:59 +0800

    feat: implemented image generation grpc service

--- backend/image-generator/main.py
+++ backend/image-generator/main.py
@@ -1,25 +1,32 @@
 from concurrent import futures
 from openai import OpenAI
+import os
+import logging
+import sys
 import grpc
 import image_generator_pb2
 import image_generator_pb2_grpc
 from grpc_reflection.v1alpha import reflection
 
-# client = OpenAI(api_key="")
-# response = client.images.generate(
-#   model="dall-e-3",
-#   prompt="a white siamese cat",
-#   size="1024x1024",
-#   quality="standard",
-#   n=1,
-# )
-#
-# image_url = response.data[0].url
 
 class ImageGeneratorService(image_generator_pb2_grpc.ImageGenerator):
     def GetImageUrl(self, request, context):
-        image_url = "test url"
+        _logger.info(f"Request received from server: {request}")
+        image_url = generateImageAndReturnImageUrl(request.prompt)
+        _logger.info(f"Generated image url: {image_url}")
         return image_generator_pb2.ImageResponse(image_url=image_url)
+
+
+def generateImageAndReturnImageUrl(prompt):
+    response = client.images.generate(
+      model="dall-e-3",
+      prompt=prompt,
+      size="1024x1024",
+      quality="standard",
+      n=1,
+    )
+
+    return response.data[0].url
 
 
 def serve():
@@ -36,5 +43,32 @@ def serve():
     server.wait_for_termination()
 
 
+def _init_logger():
+    # Create a looger named 'app'
+    logger = logging.getLogger('app')
+
+    # Set the threshold logging level of the logger to INFO
+    logger.setLevel(logging.INFO)
+
+    # Create a stream-based handler that writes the log entries into the standard output stream
+    handler = logging.StreamHandler(sys.stdout)
+
+    # Create a formatter for the logs
+    formatter = logging.Formatter('%(created)f:%(levelname)s:%(name)s:%(module)s:%(message)s')
+
+    # Set the created formatter as the formatter of the handler    
+    handler.setFormatter(formatter)
+
+    # Add the created handler to this logger
+    logger.addHandler(handler)
+
+
 if __name__ == '__main__':
+    _init_logger()
+    _logger = logging.getLogger('app')
+
+    # Initialize OpenAi client
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+    # Serve grpc server
     serve()
